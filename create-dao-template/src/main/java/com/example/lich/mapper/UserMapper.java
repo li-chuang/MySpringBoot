9b599efd9210116cc395253cbffed989ed91c6c4
lichuang <278747148@qq.com>
2021-03-28 02:44:34 +0800

    基础查询提交

--- create-dao-template/src/main/java/com/example/lich/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-//package com.example.lich.mapper;
-//
-//import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-//import com.example.lich.entity.User;
-//
-//public interface UserMapper extends BaseMapper<User> {
-//
-//}
